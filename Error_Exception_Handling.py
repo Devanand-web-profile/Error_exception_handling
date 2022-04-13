0340d252e5dbd368ce4ca6b47328b2bd009683cf
Devanand-web-profile <103214086+Devanand-web-profile@users.noreply.github.com>
2022-04-13 13:06:32 +0530

    Add files via upload

---
+++ Error_Exception_Handling.py
@@ -0,0 +1,30 @@
+# Class : 5 ... Date : 14/1/2022
+
+#  Exception Handeling   V-V-I
+# --------------------------------------------
+
+
+# What is Error ..
+
+x=12
+print(x,x) # Syntax error is Called Error...
+
+
+
+# what is Logical Error ....
+#print(x/0)   # exception
+
+'''
+a=[12,45,89]
+print(a[1])
+print(a[6])
+'''
+#-------------------------
+'''
+d={1:"ranchi"}
+print(d[9])
+'''
+x="4"
+print("Value is",x+x)
+
+
